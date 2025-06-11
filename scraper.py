92505265cc6a0d9977b1412ecf9b3c6583d041a6
Keesha Cutinha <keeshajosline@gmail.com>
2025-06-11 12:50:05 +0530

    Trigger pull request with a test comment

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ CATALOGUE_URL = BASE_URL + "catalogue/"# testing
 
 def get_soup(url):
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, timeout=10)# tester
         response.raise_for_status()
         return BeautifulSoup(response.text, "html.parser")
     except requests.exceptions.RequestException as e:
